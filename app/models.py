6c9d2a4b28a6efdfecc180786e18ded35a56f714
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-08 00:44:14 +0300

    bg:fix typing error on magic methodrepr

--- app/models.py
+++ app/models.py
@@ -2,12 +2,12 @@ import email
 from app import db
 
 class User(db.Model):
-    id=db.Column(db.Interger ,primary_key=True)
+    id=db.Column(db.Integer ,primary_key=True)
     username=db.Column(db.String(120),unique=True, index=True)
-    email=db.column(db.String(120), index=True, unique=True)
+    email=db.Column(db.String(120), index=True, unique=True)
     password_hash=db.Column(db.String(120))
 
 
-    def __repr___(self):
+    def __repr__(self):
         return '<User {} >'.format(self.username)
 
