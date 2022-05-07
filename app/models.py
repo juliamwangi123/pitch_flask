315c08f0d09cce52e07bbbc74ab8b632d9be28c5
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-07 23:27:08 +0300

    ft:add login form

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
+from turtle import title
 from app import app
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm,LoginForm
 from flask import render_template
 
 @app.route('/register')
 def regestration():
     forms=RegistrationForm()
     return render_template('regestration.html', title='regestration', form=forms)
+@app.route('/login')
+def login():
+    form=LoginForm()
+    return render_template('login.html', title='login', form=form)
