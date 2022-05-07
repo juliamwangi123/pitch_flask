cf55ecf29278039a7a96cc383c9bb2d036ac0324
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-07 21:30:25 +0300

    ft:add dummy post

--- app/route.py
+++ app/route.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/pitch')
 def index():
-    return 'hi'
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', posts=posts)
