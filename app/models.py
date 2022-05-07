1b2e7983b2fa79f1986dc4bc46213f3d8e51cade
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-08 00:38:06 +0300

    perf:create User class from db.Modeland and its field

--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
+import email
 from app import db
+
+class User(db.Model):
+    id=db.Column(db.Interger ,primary_key=True)
+    username=db.Column(db.String(120),unique=True, index=True)
+    email=db.column(db.String(120), index=True, unique=True)
+    password_hash=db.Column(db.String(120))
+
+
+    def __repr___(self):
+        return '<User {} >'.format(self.username)
+
