66bb0cc96eadff5c534ff609b24937df4b066c3d
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-08 12:58:23 +0300

    ft:add new user to database

--- app/views.py
+++ app/views.py
@@ -3,18 +3,57 @@ from flask_login import current_user, login_user , logout_user
 from app import app
 from .forms import  RegistrationForm, LoginForm
 from flask import render_template, request,flash,redirect,url_for,request
-from .route import index
 from app.models import User 
 from werkzeug.urls import url_parse
 from app import db
 
 
+from flask_login import login_required
+
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', posts=posts)
+
+
+
+
+
+
+
+
 @app.route('/register', methods=['GET','POST' ])
 def regestration():
     
-    forms=RegistrationForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form=RegistrationForm()
+
+    if request.method == "POST" and form.validate_on_submit():
+        user=User(username=form.username.data, email=form.email.data)
+        user.set_password(password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f'Account created succesufully for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+   
+    return render_template('regestration.html' ,form=form, title="Register")
+
     
-    return render_template('regestration.html', title='regestration', form=forms)
 
 
 @app.route('/login' , methods=['GET','POST' ])
