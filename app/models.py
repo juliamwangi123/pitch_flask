74537e32ee827e4bbdcb18fc7da361aa681c5590
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-08 09:52:56 +0300

    import werkzeug.security to hash password

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from email.policy import default
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id=db.Column(db.Integer ,primary_key=True)
