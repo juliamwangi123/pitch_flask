5b4190d3c937ea9bca7cb788bc05f512636338d9
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-08 08:34:57 +0300

    ft:add column that shows relationship of the pitch and the user

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     username=db.Column(db.String(120),unique=True, index=True)
     email=db.Column(db.String(120), index=True, unique=True)
     password_hash=db.Column(db.String(120))
-
+    pitch=db.relationship('Pitch' , backref='author',lazy='dynamic')
 
     def __repr__(self):
         return '<User {} >'.format(self.username)
@@ -17,6 +17,7 @@ class Pitch(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     body=db.Column(db.String(300))
     timestamp=db.Column(db.DateTime, index=True, default=datetime.now)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
     def __repr__(self):
