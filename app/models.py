3ea22f5aadd500358843a9e2277a80f075816edb
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-08 21:04:42 +0300

    ft:add migration script and  upgrade to add  newfiels to user table

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(UserMixin,db.Model):
     email=db.Column(db.String(120), index=True, unique=True)
     password_hash=db.Column(db.String(120))
     about_me=db.Column(db.String(120))
-    last_seen=db.column(db.DateTime, default=datetime.now)
+    last_seen=db.Column(db.DateTime, default=datetime.now)
     pitch=db.relationship('Pitch' , backref='author',lazy='dynamic')
 
 
