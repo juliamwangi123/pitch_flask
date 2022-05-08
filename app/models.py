89d0f7402e3121d8ff3b8711d919e9a7fc0b9591
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-08 10:18:06 +0300

    ft:add login loader

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+
+class User(UserMixin,db.Model):
     id=db.Column(db.Integer ,primary_key=True)
     username=db.Column(db.String(120),unique=True, index=True)
     email=db.Column(db.String(120), index=True, unique=True)
@@ -23,6 +26,16 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {} >'.format(self.username)
+
+#userLoader
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+    
+
+
+
 
 
 class Pitch(db.Model):
